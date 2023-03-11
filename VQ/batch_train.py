189bf3ba99678db5fb6d69e7933df857346084e6
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-03-11 10:17:51 +0800

    exp noAssoc

--- VQ/batch_train.py
+++ VQ/batch_train.py
@@ -8,9 +8,8 @@ from train_2 import is_need_train, PlusTrainer
 EXP_ROOT_PATH = '{}{}'.format(os.path.dirname(os.path.abspath(__file__)), '/exp')
 sys.path.append(EXP_ROOT_PATH)
 EXP_NAME_LIST = [
-    "2023.03.09_10vq_Zc[2]_Zs[0]_edim1_plusUnit32_singleStyle",
-    "2023.03.09_30vq_Zc[1]_Zs[0]_edim1_plusUnit32_singleStyle",
-    "2023.03.09_30vq_Zc[1]_Zs[0]_edim8_plusUnit32_singleStyle",
+    "2023.03.09_10vq_Zc[2]_Zs[0]_edim1_plusUnit32_singleStyle_noAssoc",
+    "2023.03.09_30vq_Zc[1]_Zs[0]_edim1_plusUnit32_singleStyle_noAssoc",
 ]
 EXP_NUM_LIST = ['1', '2', '3', '4', '5']
 
