38b6d5be85b6b2ea5d808b7320eca7307b048114
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-03-20 18:36:02 +0800

    Update multi_key_compare_train_test_summary.py

--- eval/multi_key_compare_train_test_summary.py
+++ eval/multi_key_compare_train_test_summary.py
@@ -1,25 +1,24 @@
 import sys
 import os
+sys.path.append('{}{}'.format(os.path.dirname(os.path.abspath(__file__)), '/../'))
 from typing import List
 from load_batch_record import ExpGroup
 from multi_key_compare import CompareGroup, plot_graph
 import numpy as np
 
-sys.path.append('{}{}'.format(os.path.dirname(os.path.abspath(__file__)), '/../'))
-
 eg1 = ExpGroup(
-        exp_name="2023.03.19_10vq_Zc[2]_Zs[0]_edim1_singleS",
-        exp_alias='w/ associative loss',
-        sub_exp=[1, 2, 3, 4, 5],
-        record_name="plus_eval.txt",
-    )
+    exp_name="2023.03.19_10vq_Zc[2]_Zs[0]_edim1_singleS",
+    exp_alias='w/ associative loss',
+    sub_exp=[1, 2, 3, 4, 5],
+    record_name="plus_eval.txt",
+)
 
 eg2 = ExpGroup(
-        exp_name="2023.03.19_10vq_Zc[2]_Zs[0]_edim1_singleS_noAssoc",
-        exp_alias='w/o associative loss',
-        sub_exp=[1, 2, 3, 4, 5],
-        record_name="plus_eval.txt",
-    )
+    exp_name="2023.03.19_10vq_Zc[2]_Zs[0]_edim1_singleS_noAssoc",
+    exp_alias='w/o associative loss',
+    sub_exp=[1, 2, 3, 4, 5],
+    record_name="plus_eval.txt",
+)
 
 exp_groups = [eg1, eg2]
 
@@ -64,11 +63,3 @@ if __name__ == '__main__':
     cg_list = gen_compare_groups(exp_groups)
     save_path = os.path.join(OUTPUT_PATH, RESULT_NAME)
     plot_graph(cg_list, save_path)
-
-
-
-
-
-
-
-
