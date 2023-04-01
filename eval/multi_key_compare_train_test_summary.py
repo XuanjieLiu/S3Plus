6226b9035af8335aaacc5f168d325d1341b3e774
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-04-01 09:52:16 +0800

    Update multi_key_compare_train_test_summary.py

--- eval/multi_key_compare_train_test_summary.py
+++ eval/multi_key_compare_train_test_summary.py
@@ -9,14 +9,14 @@ import numpy as np
 eg1 = ExpGroup(
     exp_name="2023.03.25_10vq_Zc[2]_Zs[0]_edim1_singleS",
     exp_alias='w/ associative loss',
-    sub_exp=[1, 2, 3, 4, 5],
+    sub_exp=[i for i in range(1, 21)],
     record_name="plus_eval.txt",
 )
 
 eg2 = ExpGroup(
     exp_name="2023.03.25_10vq_Zc[2]_Zs[0]_edim1_singleS_noAssoc",
     exp_alias='w/o associative loss',
-    sub_exp=[1, 2, 3, 4, 5],
+    sub_exp=[i for i in range(1, 21)],
     record_name="plus_eval.txt",
 )
 
@@ -52,7 +52,7 @@ OUTPUT_PATH = "train_test_summary/"
 EXTREME_NUM = 5
 ITER_AFTER = 8000
 Y_NAME = "Plus Accuracy (max=1.0)"
-RESULT_NAME = "3.25_1dim_compare.png"
+RESULT_NAME = "dim_1_3.25_compare.png"
 
 
 def exp_group2compare_group(exp_group: ExpGroup):
