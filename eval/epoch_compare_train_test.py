768874aaee9eeafd906901118e5bd95fb2f8ff11
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-04-04 16:30:27 +0800

    1

--- eval/epoch_compare_train_test.py
+++ eval/epoch_compare_train_test.py
@@ -22,14 +22,14 @@ from epoch_compare import EpochCompareGroup, plot_graph
 # )
 
 eg1 = ExpGroup(
-    exp_name="2023.04.03_25vq_Zc[1]_Zs[0]_edim1_singleS",
+    exp_name="2023.04.01_30vq_Zc[1]_Zs[0]_edim1_singleS",
     exp_alias='w/ associative loss',
     sub_exp=[i for i in range(1, 21)],
     record_name="plus_eval.txt",
 )
 
 eg2 = ExpGroup(
-    exp_name="2023.04.03_25vq_Zc[1]_Zs[0]_edim1_singleS_noAssoc",
+    exp_name="2023.04.01_30vq_Zc[1]_Zs[0]_edim1_singleS",
     exp_alias='w/o associative loss',
     sub_exp=[i for i in range(1, 21)],
     record_name="plus_eval.txt",
--- eval/multi_key_compare_train_test_summary.py
+++ eval/multi_key_compare_train_test_summary.py
@@ -7,14 +7,14 @@ from multi_key_compare import MultiKeyCompareGroup, plot_graph
 import numpy as np
 
 eg1 = ExpGroup(
-    exp_name="2023.04.03_25vq_Zc[1]_Zs[0]_edim1_singleS",
+    exp_name="2023.04.01_30vq_Zc[1]_Zs[0]_edim1_singleS",
     exp_alias='w/ associative loss',
     sub_exp=[i for i in range(1, 21)],
     record_name="plus_eval.txt",
 )
 
 eg2 = ExpGroup(
-    exp_name="2023.04.03_25vq_Zc[1]_Zs[0]_edim1_singleS_noAssoc",
+    exp_name="2023.04.01_30vq_Zc[1]_Zs[0]_edim1_singleS",
     exp_alias='w/o associative loss',
     sub_exp=[i for i in range(1, 21)],
     record_name="plus_eval.txt",
