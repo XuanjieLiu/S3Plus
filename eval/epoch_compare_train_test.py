ad31767f4a13e8637da3b03e9e860cf3d4f471b8
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-04-06 13:27:06 +0800

    Update epoch_compare_train_test.py

--- eval/epoch_compare_train_test.py
+++ eval/epoch_compare_train_test.py
@@ -22,14 +22,14 @@ from epoch_compare import EpochCompareGroup, plot_graph
 # )
 
 eg1 = ExpGroup(
-    exp_name="2023.04.05_25vq_Zc[1]_Zs[0]_edim8_128_singleS",
+    exp_name="2023.04.03_100vq_Zc[1]_Zs[0]_edim1_singleS",
     exp_alias='w/ associative loss',
     sub_exp=[i for i in range(1, 21)],
     record_name="plus_eval.txt",
 )
 
 eg2 = ExpGroup(
-    exp_name="2023.04.05_25vq_Zc[1]_Zs[0]_edim8_128_singleS_noAssoc",
+    exp_name="2023.04.03_100vq_Zc[1]_Zs[0]_edim1_singleS_noAssoc",
     exp_alias='w/o associative loss',
     sub_exp=[i for i in range(1, 21)],
     record_name="plus_eval.txt",
@@ -45,7 +45,7 @@ KEYS_NAME = ['TestSet']
 KEYS_STYLE = ['solid']
 OUTPUT_PATH = "train_test_epoch/"
 Y_NAME = "Plus Accuracy (max=1.0)"
-RESULT_NAME = "dim8_04.05_25vq_Zc[1]_epoch.png"
+RESULT_NAME = "dim1_04.03_100vq_Zc[1]_epoch.png"
 
 def gen_compare_groups(exp_groups: List[ExpGroup]):
     compare_groups = []
