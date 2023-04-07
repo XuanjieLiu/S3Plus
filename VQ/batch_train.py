4e26528e9fcd0ba5ede7fe1a982d0009c75e4eda
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-04-07 19:08:01 +0800

    Update batch_train.py

--- VQ/batch_train.py
+++ VQ/batch_train.py
@@ -8,7 +8,7 @@ from train import is_need_train, PlusTrainer
 EXP_ROOT_PATH = '{}{}'.format(os.path.dirname(os.path.abspath(__file__)), '/exp')
 sys.path.append(EXP_ROOT_PATH)
 EXP_NAME_LIST = [
-    "2023.04.07_100vq_Zc[1]_Zs[0]_edim8_plusUnit128_singleS",
+    "2023.04.07_100vq_Zc[1]_Zs[0]_edim8_plusUnit128_singleS_noAssoc",
 ]
 EXP_NUM_LIST = [str(i) for i in range(1, 21)]
 
