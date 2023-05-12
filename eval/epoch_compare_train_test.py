ffc9e7c25c93b18ad5c7c5f26d788550742518da
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-05-12 15:50:36 +0800

    1

--- eval/epoch_compare_train_test.py
+++ eval/epoch_compare_train_test.py
@@ -44,7 +44,8 @@ eg1 = ExpGroup(
 
 eg2 = ExpGroup(
     exp_name="2023.05.06_10vq_Zc[2]_Zs[0]_edim8_plusUnit128.2_encFc128.3_singleS_noAssoc",
-    exp_alias='w/o associative loss',
+    # exp_alias='w/o associative loss',
+    exp_alias='from 40000 epoch associative loss',
     sub_exp=[i for i in range(1, 21)],
     record_name="plus_eval.txt",
 )
