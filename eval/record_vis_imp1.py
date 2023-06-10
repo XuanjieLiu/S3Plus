1a7e3ede716081cb0d90840c2ca266d6b006999e
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-06-10 14:52:39 +0800

    1

--- eval/record_vis_imp1.py
+++ eval/record_vis_imp1.py
@@ -161,23 +161,23 @@ def eg_list2panel_input(eg_list: List[ExpGroup]):
 
 
 eg1 = ExpGroup(
-    exp_name='2023.05.26_10vq_Zc[2]_Zs[0]_edim8_plusUnit128.2_encFc128.2_singleS',
-    exp_alias='高维度(8 dim),\n 两位数,\n 多冗余(10 tokens),\n (效果好)',
-    sub_exp=[10],
+    exp_name='2023.06.09_10vq_Zc[2]_Zs[0]_edim8_plusUnit128.2_encFc128.2_plusS0.3',
+    exp_alias='高维度(8 dim),\n 两位数,\n 多冗余(10 tokens),\n (plusS0.3)',
+    sub_exp=[19],
     is_load_record=False
 )
 
 eg2 = ExpGroup(
-    exp_name='2023.05.26_10vq_Zc[2]_Zs[0]_edim8_plusUnit128.2_encFc128.2_singleS',
-    exp_alias='高维度(8 dim),\n 两位数,\n 多冗余(10 tokens),\n (效果差)',
-    sub_exp=[6],
+    exp_name='2023.06.09_10vq_Zc[2]_Zs[0]_edim8_plusUnit128.2_encFc128.2_plusS1',
+    exp_alias='高维度(8 dim),\n 两位数,\n 多冗余(10 tokens),\n (plusS1)',
+    sub_exp=[14],
     is_load_record=False
 )
 
 eg3 = ExpGroup(
-    exp_name='2023.05.26_10vq_Zc[2]_Zs[0]_edim8_plusUnit128.2_encFc128.2_singleS_noAssoc',
-    exp_alias='高维度(8 dim),\n 两位数,\n 多冗余(10 tokens),\n (无结合律)',
-    sub_exp=[14],
+    exp_name='2023.05.26_10vq_Zc[2]_Zs[0]_edim8_plusUnit128.2_encFc128.2_singleS',
+    exp_alias='高维度(8 dim),\n 两位数,\n 多冗余(10 tokens),\n (效果好)',
+    sub_exp=[10],
     is_load_record=False
 )
 
