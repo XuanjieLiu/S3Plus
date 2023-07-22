bd00573512aad36ddb9b8d0d7f0679b02b6de1c3
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-07-22 16:24:40 +0800

    1

--- VQ/exp/2023.07.09_100vq_Zc[1]_Zs[0]_edim2_[0-20]/train_config.py
+++ VQ/exp/2023.07.09_100vq_Zc[1]_Zs[0]_edim2_[0-20]/train_config.py
@@ -12,6 +12,7 @@ CONFIG = {
     'embedding_dim': 2,
     'is_plot_zc_value': True,
     'embeddings_num': 100,
+    'is_plot_vis_num': False,
     'kld_loss_scalar': 0.0,
     'checkpoint_interval': 2000,
     'learning_rate': 1e-4,
