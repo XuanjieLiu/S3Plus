8f9d1a33041a0088db1311c9264872c8f609dc67
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-10-15 13:52:53 +0800

    1

--- VQ/num_eval_batch.py
+++ VQ/num_eval_batch.py
@@ -10,12 +10,8 @@ RESULT_DIR_NAME = 'two_dim_z_vis'
 CHECK_POINT = 'checkpoint_60000.pt'
 EXP_NUM_LIST = [str(i) for i in range(1, 21)]
 EXP_NAME_LIST = [
-    '2023.09.24_100vq_Zc[1]_Zs[0]_edim2_[0-20]_noPlus',
-    '2023.09.25_100vq_Zc[1]_Zs[0]_edim2_[0-20]_plus1024_1',
-    '2023.09.25_100vq_Zc[1]_Zs[0]_edim2_[0-20]_plus1024_1_noAssoc',
-    '2023.09.24_10vq_Zc[2]_Zs[0]_edim1_[0-20]_noPlus',
-    '2023.09.24_10vq_Zc[2]_Zs[0]_edim1_[0-20]_plus16_1',
-    '2023.09.24_10vq_Zc[2]_Zs[0]_edim1_[0-20]_plus16_1_noAssoc'
+    '2023.09.24_10vq_Zc[2]_Zs[0]_edim1_[0-20]_plus1024_1',
+    '2023.09.24_10vq_Zc[2]_Zs[0]_edim1_[0-20]_plus1024_1_noAssoc'
 ]
 
 
