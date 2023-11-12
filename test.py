8cdf7f843927f5ccf47081138ee5629d7ce07ec3
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-11-12 20:19:36 +0800

    1

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import os
+import random
 import sys
 
 import sys
+import numpy as np
 
 # if len(sys.argv) < 2:
 #     print("Usage: python myscript.py arg1 arg2 ...")
@@ -17,14 +19,19 @@ import sys
 # # 输出读取到的命令行参数
 
 
-class TestA:
-    def __init__(self, a):
-        self.a = a
+# class TestA:
+#     def __init__(self, a):
+#         self.a = a
+#
+# x = TestA(3)
+# y = [x for n in range(2)]
+# for item in y:
+#     print(item.a)
+# x.a = 8
+# for item in y:
+#     print(item.a)
 
-x = TestA(3)
-y = [x for n in range(2)]
-for item in y:
-    print(item.a)
-x.a = 8
-for item in y:
-    print(item.a)
+x=int(3/2)
+print(x)
+y = random.sample(range(0, 1), 1)
+print(y)
