5cbae34908d479c3136fdcb58cdf0b80d6d1fa8f
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2023-11-16 20:09:00 +0800

    1

--- VQ/num_eval_batch.py
+++ VQ/num_eval_batch.py
@@ -4,14 +4,16 @@ sys.path.append('{}{}'.format(os.path.dirname(os.path.abspath(__file__)), '/../'
 from importlib import reload
 from num_eval import MumEval
 
-DATASET_PATH = '{}{}'.format(os.path.dirname(os.path.abspath(__file__)), '/../dataset/(0,20)-FixedPos-oneStyle')
+DATASET_PATH = '{}{}'.format(os.path.dirname(os.path.abspath(__file__)), '/../dataset/(1,20)-FixedPos-oneStyle')
 EXP_ROOT_PATH = '{}{}'.format(os.path.dirname(os.path.abspath(__file__)), '/exp')
 RESULT_DIR_NAME = 'two_dim_z_vis'
 CHECK_POINT = 'checkpoint_60000.pt'
 EXP_NUM_LIST = [str(i) for i in range(1, 21)]
 EXP_NAME_LIST = [
-    '2023.09.24_10vq_Zc[2]_Zs[0]_edim1_[0-20]_plus1024_1',
-    '2023.09.24_10vq_Zc[2]_Zs[0]_edim1_[0-20]_plus1024_1_noAssoc'
+    '2023.11.12_10vq_Zc[2]_Zs[0]_edim1_[0-20]_plus1024_1_doubleSet',
+    '2023.11.12_10vq_Zc[2]_Zs[0]_edim1_[0-20]_plus1024_1_doubleSet_noAssoc',
+    '2023.11.12_100vq_Zc[1]_Zs[0]_edim2_[0-20]_plus1024_1_doubleSet',
+    '2023.11.12_100vq_Zc[1]_Zs[0]_edim2_[0-20]_plus1024_1_doubleSet_noAssoc',
 ]
 
 
