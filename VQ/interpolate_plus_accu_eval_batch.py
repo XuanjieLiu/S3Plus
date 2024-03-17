145ec3386771787843100b0291359a53b7986536
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2024-03-17 09:45:58 +0400

    1

--- VQ/interpolate_plus_accu_eval_batch.py
+++ VQ/interpolate_plus_accu_eval_batch.py
@@ -7,7 +7,6 @@ from interpolate_plus_accu_eval import InterpolatePlusAccuEval
 from common_func import load_config_from_exp_name, record_num_list, EXP_ROOT, find_optimal_checkpoint_num_by_train_config
 
 
-
 EXP_NUM_LIST = [str(i) for i in range(1, 21)]
 EXP_NAME_LIST = [
     "2024.03.06_10vq_Zc[2]_Zs[0]_edim1_[0-20]_plus1024_1_oneShotSet_AssocCommuAll",
@@ -21,7 +20,6 @@ EXP_NAME_LIST = [
 ]
 ITP_PLUS_RECORD_PATH_ALL = 'interpolate_plus_accu_all.txt'
 ITP_PLUS_RECORD_PATH_TRAIN = 'interpolate_plus_accu_train.txt'
-
 
 
 if __name__ == "__main__":
