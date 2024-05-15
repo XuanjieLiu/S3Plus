549b750f317b7ff03cb9ff573388249f299d032c
Xuanjie Liu <liuxuanjie_13zhfx@126.com>
2024-05-15 15:23:10 +0400

    1

--- VQ/plot_multistyle_zc_batch_for_singlestyle_exp.py
+++ VQ/plot_multistyle_zc_batch_for_singlestyle_exp.py
@@ -9,9 +9,9 @@ from dataloader import SingleImgDataset
 from torch.utils.data import DataLoader
 
 
-EXP_NUM_LIST = [str(i) for i in range(1, 21)]
+EXP_NUM_LIST = [str(i) for i in range(1, 5)]
 EXP_NAME_LIST = [
-    "2024.03.17_10vq_Zc[2]_Zs[0]_edim4_[0-20]_plus1024_1_tripleSet_PureVQ"
+    "2024.05.09_256vq_Zc[1]_Zs[0]_edim8_[0-99]_plus1024_1_tripleSet_arabic_AssocFullsymmCommu"
 ]
 
 
